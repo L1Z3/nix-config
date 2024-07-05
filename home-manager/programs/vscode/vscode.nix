10f3f160c9e807544e29c2476974daea1f761b62
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-05 14:44:40 -0400

    home: id 82; system: id 44, nixos 24.05.20240702.706eef5

--- home-manager/programs/vscode/vscode.nix
+++ home-manager/programs/vscode/vscode.nix
@@ -6,7 +6,7 @@
   inherit (config.lib.file) mkOutOfStoreSymlink;
   pathToHere = "${config.home.homeDirectory}/nix/home-manager/programs/vscode";
 
-  vsCodium = false;
+  vsCodium = true;
   vsCodePackage =
     if vsCodium
     then pkgs.vscodium
