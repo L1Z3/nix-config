10c0881037c15276e15a367da558d7bdc0c27509
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-06 03:42:06 -0400

    home: id 15; system: id 8, nixos 24.05.20240702.706eef5

--- pkgs/notion-app-enhanced/default.nix
+++ pkgs/notion-app-enhanced/default.nix
@@ -13,13 +13,22 @@
     sha256 = "sha256-SqeMnoMzxxaViJ3NPccj3kyMc1xvXWULM6hQIDZySWY=";
   };
 
-  appimageContents = appimageTools.extract {inherit pname version src;};
+  polyfill-patch = "./polyfill.js";
+
+  appimageContents = (appimageTools.extract {inherit pname version src;}).overrideAttrs (oA: {
+    buildCommand = ''
+      ${oA.buildCommand}
+
+      ${asar}/bin/asar extract $out/resources/app.asar app
+      sed -e '/\"use strict\";/r${polyfill-patch}' app/renderer/preload.js > app/renderer/preload.js
+      ${asar}/bin/asar pack app $out/resources/app.asar
+    '';
+  });
 in
   appimageTools.wrapType2 {
     inherit pname version src;
-
+    
     extraInstallCommands = ''
-      # TODO: insert contents here
       install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
       substituteInPlace $out/share/applications/${pname}.desktop \
         --replace 'Exec=AppRun' 'Exec=${pname}'
