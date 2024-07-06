8914207c489ef72bbb248e5468ad2628c39b0332
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-06 04:24:11 -0400

    home: id 20; system: id 8, nixos 24.05.20240702.706eef5

--- pkgs/notion-app-enhanced/default.nix
+++ pkgs/notion-app-enhanced/default.nix
@@ -22,7 +22,7 @@
 
       ${asar}/bin/asar extract $out/resources/app.asar $out/app
       ${dos2unix}/bin/dos2unix $out/app/renderer/preload.js
-      patch -p0 $out/app/renderer/preload.js ${notion-patch}
+      patch $out/app/renderer/preload.js ${notion-patch}
       ${dos2unix}/bin/unix2dos $out/app/renderer/preload.js
       rm $out/resources/app.asar
       ${asar}/bin/asar pack $out/app $out/resources/app.asar
