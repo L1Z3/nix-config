2a0c6a265e881e5abd13ee2f9e59df7f4dda70b2
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-06 15:22:46 -0400

    home: id 43; system: id 14, nixos 24.05.20240704.c0d0be0

--- home-manager/gnome-settings.nix
+++ home-manager/gnome-settings.nix
@@ -46,32 +46,148 @@
     steal-my-focus-window
     tiling-assistant
   ];
+  customDconf = {
+    # below here is all other custom dconf entries
+
+    # enable the extensions specified above
+    "org/gnome/shell" = {
+      enabled-extensions = builtins.map (extension: extension.extensionUuid) extensions;
+    };
+
+    # set gnome background to blobs
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file://${pkgs.gnome.gnome-backgrounds}/share/backgrounds/gnome/blobs-l.svg";
+      picture-uri-dark = "file://${pkgs.gnome.gnome-backgrounds}/share/backgrounds/gnome/blobs-d.svg";
+      primary-color = "#241f31";
+      secondary-color = "#000000";
+    };
+
+    "org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+      show-battery-percentage = true;
+    };
+
+    # fix dark mode in gtk3 apps
+    # "org/gnome/desktop/interface" = {
+    #   color-scheme = "prefer-dark";
+    #   gtk-theme = "adw-gtk3-dark";
+    # };
+
+    # caps lock backspace remap, plus double shift to caps lock
+    "org/gnome/desktop/input-sources" = {
+      xkb-options = ["shift:both_capslock" "lv3:ralt_switch" "caps:backspace"];
+    };
+
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "default";
+      natural-scroll = false;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      disable-while-typing = false;
+      edge-scrolling-enabled = false;
+      natural-scroll = false;
+      speed = 0.28395061728395055;
+      tap-to-click = true;
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/screensaver" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file://${pkgs.gnome.gnome-backgrounds}/share/backgrounds/gnome/blobs-l.svg";
+      primary-color = "#241f31";
+      secondary-color = "#000000";
+    };
+
+    "org/gnome/desktop/sound" = {
+      allow-volume-above-100-percent = true;
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      maximize = [];
+      move-to-workspace-1 = ["<Super>1"];
+      move-to-workspace-2 = ["<Super>2"];
+      move-to-workspace-3 = ["<Super>3"];
+      move-to-workspace-4 = ["<Super>4"];
+      switch-applications = ["<Super>Tab"];
+      switch-applications-backward = ["<Shift><Super>Tab"];
+      switch-to-workspace-1 = ["<Super>a"];
+      switch-to-workspace-2 = ["<Super>s"];
+      switch-to-workspace-3 = ["<Super>d"];
+      switch-to-workspace-4 = ["<Super>f"];
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Shift><Alt>Tab"];
+      toggle-fullscreen = ["<Control><Alt>Home"];
+      unmaximize = [];
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+      num-workspaces = 4;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+      edge-tiling = false;
+      workspaces-only-on-primary = false;
+    };
+
+    "org/gnome/mutter/keybindings" = {
+      toggle-tiled-left = [];
+      toggle-tiled-right = [];
+    };
+
+    "org/gnome/nautilus/compression" = {
+      default-compression-format = "zip";
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      show-create-link = true;
+    };
+
+    "org/gnome/shell/extensions/net/gfxmonk/impatience" = {
+      speed-factor = 0.5;
+    };
+
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = true;
+    };
+
+    "org/gnome/shell/extensions/appindicator" = {
+      legacy-tray-enabled = false;
+    };
+
+    "org/gnome/shell/extensions/clipboard-history" = {
+      display-mode = 0;
+      history-size = 20;
+      next-entry = ["<Super>period"];
+      prev-entry = ["<Super>comma"];
+      window-width-percentage = 20;
+    };
+
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      toggle-application-view = [];
+      toggle-overview = [];
+      toggle-quick-settings = [];
+    };
+
+    "org/gnome/shell/overrides" = {
+      edge-tiling = false;
+    };
+  };
 in
   # nautilus bookmarks
   with lib.hm.gvariant; {
     home.packages = packages ++ extensions;
 
     gtk.gtk3.bookmarks = nautilusBookmarks;
-
-    # autostart programs implementation
-    home.file = builtins.listToAttrs (map
-      (pkg: {
-        name = ".config/autostart/" + pkg.pname + ".desktop";
-        value =
-          if pkg ? desktopItem
-          then {
-            # Application has a desktopItem entry.
-            # Assume that it was made with makeDesktopEntry, which exposes a
-            # text attribute with the contents of the .desktop file
-            text = pkg.desktopItem.text;
-          }
-          else {
-            # Application does *not* have a desktopItem entry. Try to find a
-            # matching .desktop name in /share/apaplications
-            source = pkg + "/share/applications/" + pkg.pname + ".desktop";
-          };
-      })
-      autostartPrograms);
 
     # jank workaround so new home.packages appear in gnome search without logging out
     # TODO figure out how to make lib.mkAfter work here
@@ -110,6 +226,26 @@ in
         '';
       };
     };
+
+    # autostart programs implementation
+    home.file = builtins.listToAttrs (map
+      (pkg: {
+        name = ".config/autostart/" + pkg.pname + ".desktop";
+        value =
+          if pkg ? desktopItem
+          then {
+            # Application has a desktopItem entry.
+            # Assume that it was made with makeDesktopEntry, which exposes a
+            # text attribute with the contents of the .desktop file
+            text = pkg.desktopItem.text;
+          }
+          else {
+            # Application does *not* have a desktopItem entry. Try to find a
+            # matching .desktop name in /share/apaplications
+            source = pkg + "/share/applications/" + pkg.pname + ".desktop";
+          };
+      })
+      autostartPrograms);
 
     # this first part is the implementation of custom keybinds
     dconf.settings =
@@ -160,141 +296,7 @@ in
               ]);
         in
           mkSettings customKeybinds)
-        {
-          # below here is all other custom dconf entries
-
-          # enable the extensions specified above
-          "org/gnome/shell" = {
-            enabled-extensions = builtins.map (extension: extension.extensionUuid) extensions;
-          };
-
-          # set gnome background to blobs
-          "org/gnome/desktop/background" = {
-            color-shading-type = "solid";
-            picture-options = "zoom";
-            picture-uri = "file://${pkgs.gnome.gnome-backgrounds}/share/backgrounds/gnome/blobs-l.svg";
-            picture-uri-dark = "file://${pkgs.gnome.gnome-backgrounds}/share/backgrounds/gnome/blobs-d.svg";
-            primary-color = "#241f31";
-            secondary-color = "#000000";
-          };
-
-          "org/gnome/desktop/interface" = {
-            enable-hot-corners = false;
-            show-battery-percentage = true;
-          };
-
-          # fix dark mode in gtk3 apps
-          # "org/gnome/desktop/interface" = {
-          #   color-scheme = "prefer-dark";
-          #   gtk-theme = "adw-gtk3-dark";
-          # };
-
-          # caps lock backspace remap, plus double shift to caps lock
-          "org/gnome/desktop/input-sources" = {
-            xkb-options = ["shift:both_capslock" "lv3:ralt_switch" "caps:backspace"];
-          };
-
-          "org/gnome/desktop/peripherals/mouse" = {
-            accel-profile = "default";
-            natural-scroll = false;
-          };
-
-          "org/gnome/desktop/peripherals/touchpad" = {
-            disable-while-typing = false;
-            edge-scrolling-enabled = false;
-            natural-scroll = false;
-            speed = 0.28395061728395055;
-            tap-to-click = true;
-            two-finger-scrolling-enabled = true;
-          };
-
-          "org/gnome/desktop/screensaver" = {
-            color-shading-type = "solid";
-            picture-options = "zoom";
-            picture-uri = "file:///usr/share/backgrounds/gnome/blobs-l.svg";
-            primary-color = "#241f31";
-            secondary-color = "#000000";
-          };
-
-          "org/gnome/desktop/sound" = {
-            allow-volume-above-100-percent = true;
-          };
-
-          "org/gnome/desktop/wm/keybindings" = {
-            maximize = [];
-            move-to-workspace-1 = ["<Super>1"];
-            move-to-workspace-2 = ["<Super>2"];
-            move-to-workspace-3 = ["<Super>3"];
-            move-to-workspace-4 = ["<Super>4"];
-            switch-applications = ["<Super>Tab"];
-            switch-applications-backward = ["<Shift><Super>Tab"];
-            switch-to-workspace-1 = ["<Super>a"];
-            switch-to-workspace-2 = ["<Super>s"];
-            switch-to-workspace-3 = ["<Super>d"];
-            switch-to-workspace-4 = ["<Super>f"];
-            switch-windows = ["<Alt>Tab"];
-            switch-windows-backward = ["<Shift><Alt>Tab"];
-            toggle-fullscreen = ["<Control><Alt>Home"];
-            unmaximize = [];
-          };
-
-          "org/gnome/desktop/wm/preferences" = {
-            button-layout = "appmenu:minimize,maximize,close";
-            num-workspaces = 4;
-          };
-
-          "org/gnome/mutter" = {
-            dynamic-workspaces = false;
-            edge-tiling = false;
-            workspaces-only-on-primary = false;
-          };
-
-          "org/gnome/mutter/keybindings" = {
-            toggle-tiled-left = [];
-            toggle-tiled-right = [];
-          };
-
-          "org/gnome/nautilus/compression" = {
-            default-compression-format = "zip";
-          };
-
-          "org/gnome/nautilus/preferences" = {
-            show-create-link = true;
-          };
-
-          "org/gnome/shell/extensions/net/gfxmonk/impatience" = {
-            speed-factor = 0.5;
-          };
-
-          "org/gnome/shell/app-switcher" = {
-            current-workspace-only = true;
-          };
-
-          "org/gnome/shell/extensions/appindicator" = {
-            legacy-tray-enabled = false;
-          };
-
-          "org/gnome/shell/extensions/clipboard-history" = {
-            display-mode = 0;
-            history-size = 20;
-            next-entry = ["<Super>period"];
-            prev-entry = ["<Super>comma"];
-            window-width-percentage = 20;
-          };
-
-          "org/gnome/shell/keybindings" = {
-            switch-to-application-1 = [];
-            switch-to-application-2 = [];
-            switch-to-application-3 = [];
-            switch-to-application-4 = [];
-            toggle-application-view = [];
-            toggle-overview = [];
-            toggle-quick-settings = [];
-          };
-
-          "org/gnome/shell/overrides" = {
-            edge-tiling = false;
-          };
-        }
+        # add custom dconf declared above
+        customDconf
       ];
   }
