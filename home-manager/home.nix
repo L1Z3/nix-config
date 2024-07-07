aec9e10e680ab7ac731410a348e441f785db6dc1
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-06 23:33:43 -0400

    home: id 71; system: id 31, nixos 24.05.20240704.c0d0be0

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,6 +18,7 @@
     ./gnome-settings.nix
     ./programs/vscode
     ./programs/htop
+    ./programs/syncplay
   ];
   # TODO list:
   #    figure out how to set profile image for gnome user
@@ -141,7 +142,6 @@
     audacity
     gimp
     deluge
-    syncplay
 
     # system tools
     qpwgraph
---
+++ home-manager/programs/syncplay/default.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  pathToHere = "${config.home.homeDirectory}/nix/home-manager/programs/syncplay";
+in {
+  home.packages = with pkgs; [
+    syncplay
+  ];
+
+  xdg.configFile = {
+    # TODO make the path relative to flake dir somehow (still needs to expand to absolute path for nix reasons)
+    "syncplay.ini".source = mkOutOfStoreSymlink "${pathToHere}/syncplay.ini";
+  };
+}
