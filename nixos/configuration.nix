3f7e2be469f9b64cc9cd2167f4efe9c1d4a367ea
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-06 23:41:26 -0400

    enable kernel stuff for obs virtual camera
    home: id 71; system: id 33, nixos 24.05.20240704.c0d0be0
    
    home: id 71; system: id 34, nixos 24.05.20240704.c0d0be0
    
    home: id 71; system: id 35, nixos 24.05.20240704.c0d0be0

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -208,6 +208,20 @@
     curl
   ];
 
+  # enable kernel stuff for obs virtual camera
+  # boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+  # boot.kernelModules = [
+  #   "v4l2loopback"
+  # ];
+  # OR these might be needed instead; TODO reboot and test which one works for obs
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
