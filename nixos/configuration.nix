0531a75a31525885e325df5d74db19be19b38d45
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-06 23:43:41 -0400

    home: id 71; system: id 33, nixos 24.05.20240704.c0d0be0

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -209,10 +209,17 @@
   ];
 
   # enable kernel stuff for obs virtual camera
-  boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
-  boot.kernelModules = [
-    "v4l2loopback"
+  # boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+  # boot.kernelModules = [
+  #   "v4l2loopback"
+  # ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
   ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
