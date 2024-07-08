4f0a5a44c7cd8935a0e77eb25a9b3ac9d5727fb5
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-08 17:56:35 -0400

    enable network printer discovery

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,6 +74,12 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  # enable network printer discovery
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
