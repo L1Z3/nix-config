ca6c60f9353532aa462ca32bc151c492750c8a3c
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-08 12:51:26 -0400

    home: id 118; system: id 44, nixos 24.05.20240707.1948467

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,8 +19,6 @@
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
-    # gnome icon module
-    # ./gnome-icon.nix
   ];
 
   # Bootloader.
@@ -163,7 +161,6 @@
       ];
       # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["networkmanager" "wheel" "ydotool"];
-      # icon = ../media/madeline.jpg;
     };
   };
 
