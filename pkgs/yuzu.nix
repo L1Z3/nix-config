5fe5cb5ef66345f430964dd148f17dba7a46ddca
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-16 16:22:02 -0400

    home: id 136; system: id 62, nixos 24.05.20240709.249fbde

--- pkgs/yuzu.nix
+++ pkgs/yuzu.nix
@@ -3,13 +3,25 @@
   appimageTools,
   ...
 }: let
-  appimage-file = fetchurl {
+  pname = "yuzu";
+  version = "EA-4176";
+
+  src = fetchurl {
     url = "https://archive.org/download/yuzu-windows-msvc-20240304-537296095_20240305_1340/Linux/Linux-Yuzu-EA-4176.AppImage";
     sha256 = "sha256-bUTVL8br2POy5HB1FszlNQNChdRWcwIlG6/RCceXIlg=";
   };
+
+  appimage-contents = appimageTools.extract {
+    inherit pname version src;
+  };
+
+  desktop-file-name = "org.yuzu_emu.yuzu";
 in
-  appimageTools.wrapType2 {
-    name = "yuzu";
-    version = "EA-4176";
-    src = appimage-file;
+  appimageTools.wrapAppImage {
+    inherit pname version;
+    src = appimage-contents;
+    extraInstallCommands = ''
+      install -m 444 -D ${appimage-contents}/${desktop-file-name}.desktop -t $out/share/applications
+      cp -r ${appimage-contents}/usr/share/icons $out/share
+    '';
   }
