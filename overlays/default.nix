0dcd58438b12aede0297ac1416e4a67955bd20fb
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2024-07-18 14:29:38 -0400

    home: id 169; system: id 90, nixos 24.05.20240714.53e81e7

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,14 +3,44 @@
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs final.pkgs;
 
+  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.unstable'
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+
+  # same, but for master branch (if we enabled master in flake.nix)
+  ${
+    if inputs ? nixpkgs-master
+    then "master-packages"
+    else null
+  } = final: _prev: {
+    master = import inputs.nixpkgs-master {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+
+  # local repo dev packages (for my WIP changes)
+  dev-packages = final: _prev: {
+    dev = import inputs.nixpkgs-dev {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
     # don't spam logs with another window session manager logs
-    unstable = final.unstable.overrideScope (selfu: superu: {
+    unstable = prev.unstable.overrideScope (selfu: superu: {
       gnomeExtensions = superu.gnomeExtensions.overrideScope (selfge: superge: {
-        another-window-session-manager = superge.another-window-session-manager.overrideAttrs (oldAttrs: rec {
+        # gnomeExtensions = prev.gnomeExtensions.overrideScope (selfge: superge: {
+        another-window-session-manager = prev.unstable.gnomeExtensions.another-window-session-manager.overrideAttrs (oldAttrs: rec {
           patches =
             (oldAttrs.patches or [])
             ++ [
@@ -89,34 +119,5 @@
     #       '';
     #     });
     # });
-  };
-
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
-  };
-
-  # same, but for master branch (if we enabled master in flake.nix)
-  ${
-    if inputs ? nixpkgs-master
-    then "master-packages"
-    else null
-  } = final: _prev: {
-    master = import inputs.nixpkgs-master {
-      system = final.system;
-      config.allowUnfree = true;
-    };
-  };
-
-  # local repo dev packages (for my WIP changes)
-  dev-packages = final: _prev: {
-    dev = import inputs.nixpkgs-dev {
-      system = final.system;
-      config.allowUnfree = true;
-    };
   };
 }
