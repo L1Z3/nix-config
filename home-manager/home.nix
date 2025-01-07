5ca7108ebd9e3527ca9b9877a33b2cb268a89ceb
L1Z3 <10276179+L1Z3@users.noreply.github.com>
2025-01-07 13:58:25 -0500

    rolling back zoom-us because it broke again

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -322,7 +322,15 @@ in {
     obsidian
     xournalpp
     # TODO fix screen sharing
-    unstable.zoom-us
+    # unstable.zoom-us
+    # zoom won't launch... rolling back
+    (pkgs.unstable.zoom-us.overrideAttrs {
+      version = "6.2.11.5069";
+      src = pkgs.fetchurl {
+        url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+        hash = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+      };
+    })
 
     # game stuff
     unstable.prismlauncher
