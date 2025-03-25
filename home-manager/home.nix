a684d281b95cad040b6be13297dbb0be9628c332
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2025-03-25 14:32:34 -0400

    switch to nix rustdesk
    home: id 662; system: id 488, nixos 24.11.20250320.7105ae3

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -380,6 +380,7 @@ in {
     # fastx-client # silly little custom package # TODO upstream
     anydesk
     warpinator-fixed # patched version to add pillow TODO upstream
+    rustdesk
 
     # messaging
     vesktop
@@ -430,7 +431,7 @@ in {
       "com.github.tchx84.Flatseal"
       "org.zulip.Zulip"
       "com.steamgriddb.SGDBoop"
-      "com.rustdesk.RustDesk"
+      # "com.rustdesk.RustDesk"
       # so many things are broken on nix zoom right now, so flatpak it is
       "us.zoom.Zoom"
       # "org.x.Warpinator"
