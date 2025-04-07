0af9b08df2fcb150bc14215dec03b8e96727c4f5
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2025-04-07 17:11:24 -0400

    temp vmware fix + bump flake inputs

--- overlays/default.nix
+++ overlays/default.nix
@@ -40,14 +40,29 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # don't spam logs with another window session manager logs
-    gnomeExtensions =
-      prev.gnomeExtensions
-      // {
-        unstable-another-window-session-manager-patched = final.unstable.gnomeExtensions.another-window-session-manager.overrideAttrs (oldAttrs: rec {
-          patches =
-            (oldAttrs.patches or []) ++ [./dont-spam-log.patch];
-        });
+    # fix vmware download thing temporarily (https://github.com/NixOS/nixpkgs/issues/392841)
+    vmware-workstation = let
+      finalAttrs = final.vmware-workstation;
+      version = "17.6.1";
+      build = "24319023";
+      baseUrl = "https://web.archive.org/web/20241105192443if_/https://softwareupdate.vmware.com/cds/vmw-desktop/ws/${version}/${build}/linux";
+      vmware-unpack-env = prev.buildFHSEnv {
+        pname = "vmware-unpack-env";
+        inherit version;
+        targetPkgs = pkgs: [pkgs.zlib];
+      };
+    in
+      prev.vmware-workstation.overrideAttrs {
+        src =
+          prev.fetchzip {
+            url = "${baseUrl}/core/VMware-Workstation-${version}-${build}.x86_64.bundle.tar";
+            hash = "sha256-VzfiIawBDz0f1w3eynivW41Pn4SqvYf/8o9q14hln4s=";
+            stripRoot = false;
+          }
+          + "/VMware-Workstation-${version}-${build}.x86_64.bundle";
+        unpackPhase = ''
+          ${vmware-unpack-env}/bin/vmware-unpack-env -c "sh ${finalAttrs.src} --extract unpacked"
+        '';
       };
 
     # TODO fix
