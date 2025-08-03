f043af05d21acb0c3a162232c3829cf8d5d5f257
Elizabeth Jones <10276179+L1Z3@users.noreply.github.com>
2025-08-03 15:37:17 -0400

    home: id 745; system: id 657, nixos 25.11.20250731.94def63

--- nixos/modules/hyprland/default.nix
+++ nixos/modules/hyprland/default.nix
@@ -20,29 +20,28 @@ in {
   # ly + uwsm failed to start hyprland, so we use tuigreet for now
   services.greetd = {
     enable = true;
-    # restart = true;
-    # vt = 1; # tty
+    restart = true;
     settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'uwsm start hyprland-uwsm.desktop'";
-        user = "greeter";
-      };
       # default_session = {
-      #   command = "${pkgs.greetd.tuigreet}/bin/tuigreet -w 69 -t --time-format '%B, %A %d @ %H:%M:%S' -r --remember-session --asterisks --user-menu -g 'Password: ********' --container-padding 1 --prompt-padding 0 --theme 'border=magenta;text=white;prompt=cyan;time=green;action=yellow;button=red;container=black;input=white'";
+      #   command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'uwsm start hyprland-uwsm.desktop'";
       #   user = "greeter";
       # };
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd 'uwsm start hyprland-uwsm.desktop' -w 69 -t --time-format '%B, %A %d @ %H:%M:%S' -r --remember-session --asterisks --user-menu -g 'Password: ********' --container-padding 1 --prompt-padding 0 --theme 'border=magenta;text=white;prompt=cyan;time=green;action=yellow;button=red;container=black;input=white'";
+        user = "greeter";
+      };
     };
   };
   # optional below
-  # systemd.services.greetd.serviceConfig = {
-  #   Type = "idle";
-  #   StandardInput = "tty";
-  #   StandardOutput = "null"; # no tty spam
-  #   StandardError = "journal";
-  #   TTYReset = true;
-  #   TTYVHangup = true;
-  #   TTYVTDisallocate = true;
-  # };
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "null"; # no tty spam
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
   # programs.uwsm = {
   #   enable = true;
   #   waylandCompositors = {
